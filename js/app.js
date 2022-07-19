d519eace5d4689008763c99fc08363d3463fad95
mazzaronecristian <cristian.mazzarone@stud.unifi.it>
2022-07-19 16:04:17 +0200

    plot peso

--- js/app.js
+++ js/app.js
@@ -79,9 +79,9 @@ function closeField(button){
 
 //END SCRIPT FOR POP-UP
 
-	//START NAVBAR SCRIP
-	const food_btn = document.querySelector("#food-btn");
-	const health_btn = document.querySelector("#health-btn");
+//START NAVBAR SCRIP
+	const food_btn = document.getElementById("food-btn");
+	const health_btn = document.getElementById("health-btn");
 	const health_info = document.querySelector("#health-info");
 	const food_info = document.querySelector("#food-info")
 
@@ -90,12 +90,52 @@ function closeField(button){
 		health_info.classList.remove("avoid-clicks");
 		food_info.classList.remove("info-active");
 		food_info.classList.add("avoid-clicks");
-	})
+	});
+
 
 	food_btn.addEventListener('click', ()=>{
 		food_info.classList.add("info-active");
 		food_info.classList.remove("avoid-clicks");
 		health_info.classList.remove("info-active");
 		health_info.classList.add("avoid-clicks");
-})
+	});
+
 //END NAVBAR SCRIPT
+
+//START PLOT SCRIPT
+var values = [0];
+var labels = [0];
+
+function pickAndPlotValues(idValue, idLabel) {
+	let field_value = document.getElementById(idValue);
+	let field_label = document.getElementById(idLabel);
+	console.log(field_label.value);
+
+	if(field_value.value != "" && field_label.value != "") {
+		if (values[0]==0) 
+			values[0] = field_value.value;
+		else 
+			values.push(eval(field_value.value));
+		if(labels[0] == 0) 
+			labels[0] = field_label.value;
+		else 
+			labels.push(field_label.value);
+		
+		new Chart("plotPeso", {
+		  type: "line",
+		  data: {
+		    labels: labels,
+		    datasets: [{
+		      data: values,
+		      borderColor: "#577590",
+		      fill: false
+		    }]
+		  },
+		  options: {
+		    legend: {display: false}
+		  }
+		});
+	}
+	else return;
+}
+//END PLOT SCRIPT
