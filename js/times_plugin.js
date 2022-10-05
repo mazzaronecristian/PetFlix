2a16efc58f960248024ff70b7410d569ee3583db
mazzaronecristian <cristian.mazzarone@stud.unifi.it>
2022-10-05 14:52:09 +0200

    separazione js per plugin

---
+++ js/times_plugin.js
@@ -0,0 +1,74 @@
+(function($){
+	$.fn.times = function(options){
+
+		return this.each(function(i, obj) {
+			let $this = $(this);
+			load_pop_up($this);
+			let $openButton = $('.open', $this);//$this = .selezioneOrari
+			let $addButton = $('.add', $this);
+			let $closeButton = $('.close-button', $this);
+			let $overlay = $('.overlay', $this);
+
+			$openButton.on('click', function(){
+				$this.find('.modal').addClass('active');
+				$this.find('.overlay').addClass('active');
+			});
+
+			$addButton.on('click', function(){
+				let $field = $this.find('.newTimes');
+				let html = 	"<div class=\"time\"><input class=\"time-field\" type=\"time\" value=\"00:00\"><button onclick=\"closeField(this)\" class=\"edit remove\"><i class=\"fa-solid fa-minus\"></i></button></div>";
+
+				$field.append(html);
+			});
+
+			$closeButton.on('click', function(){
+				close_pop_up($this);
+			});
+
+			$overlay.on('click', function(){
+				close_pop_up($this);
+			});
+
+		});
+
+		function load_pop_up($el) {
+			let html = '<button data-modal-target="#modalFood" class="edit open"><i class="fa-solid fa-pencil"></i></button>';
+			html += '<div class="modal" id="modalFood">'+
+						'<div class="modal-header">'+
+							'<div class="title">Modifica gli orari</div>'+
+							'<button data-close-button class="close-button"><i class="fa-solid fa-xmark"></i></button>'+
+						'</div>'+
+						'<div class="modal-body">'+
+		 					'<form class="newTimes">'+
+
+							'</form>'+
+							'<button data-add-time-field class="edit add"><i class="fa-solid fa-plus"></i></button>'+		
+						'</div>'+	
+					'</div>'+ 
+					'<div class="overlay"></div>';
+			$el.append(html);
+		}	
+
+		function close_pop_up($el){
+			$el.find('.modal').removeClass('active');
+			$el.find('.overlay').removeClass('active');
+			let $form = $el.find('.newTimes');
+			var input = $('input.time-field', $form);
+			let html = '';
+			
+			for (var i = 0; i < input.length; i++) {
+				html += '<li>'+$form.find(input[i]).val()+'</li>';
+			}
+
+			let $times = $el.find('ul.times');
+			$times.append(html);
+
+		}
+
+	}
+})(jQuery);
+
+function closeField(button){
+	let field = button.closest('.time')
+	field.remove()
+}
---
+++ js/times_plugin_init.js
@@ -0,0 +1,4 @@
+jQuery(document).ready(function(){
+	console.log("TIMES PLUGIN IS READY");
+	jQuery(".sezioneOrari").times({serverURL : "server/actions.php"});
+});
