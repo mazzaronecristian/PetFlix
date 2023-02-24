0a538006402dd70404672cf2dc90cf155e2bad55
mazzaronecristian <cristian.mazzarone@stud.unifi.it>
2023-02-24 10:31:54 +0100

    nuovi-pesi display flex nowrap

--- body.php
+++ body.php
@@ -4,11 +4,8 @@
       <div class="menu">
         <button id="food-btn" class="btn transparent">Cibo</button>
         <button id="health-btn" class="btn transparent">Salute</button>
-        <button
-          id="sos-btn"
-          class="btn"
-          onclick="location.href='https://www.google.com/maps/search/veterinario+vicino+a+me'"
-        >
+        <button id="sos-btn" class="btn"
+          onclick="location.href='https://www.google.com/maps/search/veterinario+vicino+a+me'">
           SOS
         </button>
       </div>
@@ -23,7 +20,7 @@
           <h2>Seleziona la taglia del tuo animale</h2>
           <form action="server/actionsSize.php" method="post">
             <select id="101" class="selector">
-            <option value="0">seleziona taglia</option>
+              <option value="0">seleziona taglia</option>
               <option value="1">Piccola</option>
               <option value="2">Media</option>
               <option value="3">Grande</option>
@@ -34,8 +31,8 @@
           <div class="serviceAbilitator">
             <h2>Pianifica orari</h2>
             <label class="switch">
-                  <input class="switch" id="102" type="checkbox">
-                <span class="slider"></span>
+              <input class="switch" id="102" type="checkbox">
+              <span class="slider"></span>
             </label>
           </div>
 
@@ -52,14 +49,17 @@
       <div class="info-container">
         <div class="peso plot-peso">
           <h2>Controllo peso</h2>
-          <form>
-            <label>inserisci peso: </label>
-            <input class="nuovoPeso" type="text" name="peso" placeholder="kg" />
-            <input class="nuovaData" type="date" name="data" />
-          </form>
-          <button class="invio-peso edit">
-            <i class="fa-solid fa-file-import"></i>
-          </button>
+          <div class="nuovi-pesi">
+            <form>
+              <label>inserisci peso: </label>
+              <input class="nuovoPeso" type="text" name="peso" placeholder="kg" />
+              <input class="nuovaData" type="date" name="data" />
+            </form>
+            <button class="invio-peso edit">
+              <i class="fa-solid fa-file-import"></i>
+            </button>
+          </div>
+
           <h2>Grafico</h2>
           <canvas id="plotPeso" style="width: 100%; max-width: 700px"></canvas>
         </div>
@@ -68,7 +68,7 @@
           <div class="serviceAbilitator">
             <h2>Programma uscite</h2>
             <label class="switch">
-              <input class="switch" id="202" type="checkbox">                
+              <input class="switch" id="202" type="checkbox">
               <span class="slider"></span>
             </label>
           </div>
