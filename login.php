e576d5d23a16d679a3d48feb27d546e64d941603
mazzaronecristian <cristian.mazzarone@stud.unifi.it>
2023-03-14 12:03:13 +0100

    implementazione cookie

--- login.php
+++ login.php
@@ -1,13 +1,29 @@
+<?php 
+    $cookie_name = "username";
+    $cookie_value = "pippo";
+    setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+
+    $cookie_name = "password";
+    $cookie_value = "password";
+    setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+
+    if(isset($_COOKIE["username"])&&isset($_COOKIE["password"])){
+        session_start();
+        $_SESSION['name'] = $_COOKIE["username"];
+        header("Location: index.php");
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <?php include('commonHeader.php') ?>
 
-    <title>Document</title>
+    <title>PetFlix</title>
 </head>
 
 <body>
+
     <h2>prova prova</h2>
 </body>
 
