cb6010ba50976d382254f24ee2a28a23db91188c
mazzaronecristian <cristian.mazzarone@stud.unifi.it>
2023-03-14 11:37:20 +0100

    commonHeader.php

---
+++ commonHeader.php
@@ -0,0 +1,9 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap"
+    rel="stylesheet" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+<script src="https://kit.fontawesome.com/13645b992e.js" crossorigin="anonymous"></script>
+<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
--- index.php
+++ index.php
@@ -1,33 +1,23 @@
 <?php
-  require("server/session.php");
+require("server/session.php");
 ?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8" />
-    <link rel="stylesheet" type="text/css" href="css/foodStyle.css" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="apple-mobile-web-app-capable" content="yes" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap"
-      rel="stylesheet"
-    />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
-      <script
-      src="https://kit.fontawesome.com/13645b992e.js"
-      crossorigin="anonymous"
-    ></script>
-    <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
-    <script type="text/javascript" src="js/times_plugin_init.js"></script>
-    <script type="text/javascript" src="js/times_plugin.js"></script>
-    <script defer type="text/javascript" src="js/app.js"></script>
-    <script type="text/javascript" src="js/options_init.js"></script>
-    <script type="text/javascript" src="js/options.js"></script>
-    <script type="text/javascript" src="js/plot_init.js"></script>
-    <script type="text/javascript" src="js/plot.js"></script>
-    <title>PetFlix</title>
-  </head>
 
-  <?php include('body.php') ?>
+<head>
+  <?php include('commonHeader.php') ?>
+
+  <link rel="stylesheet" type="text/css" href="css/foodStyle.css" />
+  <script type="text/javascript" src="js/times_plugin_init.js"></script>
+  <script type="text/javascript" src="js/times_plugin.js"></script>
+  <script defer type="text/javascript" src="js/app.js"></script>
+  <script type="text/javascript" src="js/options_init.js"></script>
+  <script type="text/javascript" src="js/options.js"></script>
+  <script type="text/javascript" src="js/plot_init.js"></script>
+  <script type="text/javascript" src="js/plot.js"></script>
+  <title>PetFlix</title>
+</head>
+
+<?php include('body.php') ?>
+
 </html>
